5bc373dd9500ce15827fd3ff77898cefb6515894
besumero628 <besumero628@gmail.com>
2021-09-05 15:00:12 +0000

    [Update] application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,18 @@ module LABONITY
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    
+    config.time_zone = "Tokyo"
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
     config.i18n.default_locale = :ja
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.assets false
+      g.test_framework :rspec
+      g.controller_spec false
+      g.view_space false
+    end
   end
 end
