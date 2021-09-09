8bc723f5f1a4172dfa44408ec8826cbb0cd628a4
besumero628 <besumero628@gmail.com>
2021-09-09 14:58:12 +0000

    [Update] News Rspec

---
+++ spec/features/public/news_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+feature "News Release" do
+  scenario "release前のnewsにはアクセスできない。" do
+    travel_to (Time.new(2020,12,30))
+    news = create(:news)
+    
+    travel_to (Time.new(2020,12,31))
+    visit public_news_path(news.id)
+    
+    expect(current_path).to eq public_root_path
+  end
+  
+  scenario "release後のnewsにはアクセスできる。" do
+    travel_to (Time.new(2020,12,31))
+    news = create(:news)
+    
+    travel_to (Time.new(2021,1,2))
+    visit public_news_path(news.id)
+    
+    expect(current_path).to eq public_news_path(news.id)
+  end
+  
+end
--- spec/models/news_spec.rb
+++ spec/models/news_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe News, type: :model do
   describe "validation" do
-    example "releaseの日付は現在時刻を超えたものしか登録できない。" do
-      news = build(:news, release_at: Time.current)
+    example "releaseの日付は現在時刻より前は登録できない。" do
+      travel_to (Time.new(2021,1,2))
+      news = build(:news)
+      
+      expect(news).not_to be_valid
     end
   end
 end
