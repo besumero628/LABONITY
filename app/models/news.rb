967d9170b5b4ed42d09c5684de084c64726c3e47
besumero628 <besumero628@gmail.com>
2021-09-09 02:24:01 +0000

    [Add] news seed

---
+++ app/models/news.rb
@@ -0,0 +1,14 @@
+class News < ApplicationRecord
+  validates :title, :body, :release_at, presence: true
+  # validate :release_check
+ 
+    # private
+    # # releaseが現在時刻以降かvalidation
+    # def release_check
+    #     if Time.now >= release_at
+    #         errors.add(:release_at, "を設定する場合は、現在時刻以降を指定してください。")
+    #     end
+    # end
+  
+  
+end
