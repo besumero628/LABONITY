6defe84274d7b5e11d02da12bca278807c518b6c
besumero628 <besumero628@gmail.com>
2021-09-16 20:36:41 +0000

    setting

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.precompile += %w(public.css admin.css company_admin.css bootstrap.min.css jquery.min.js)
+Rails.application.config.assets.precompile += %w(public.css admin.css company_admin.css now-ui-kit.css)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
