7e66beeef0e8a1323bb2323dc765768e51cd2f82
besumero628 <besumero628@gmail.com>
2021-09-18 12:53:52 +0000

    index/new page

--- app/controllers/public/communities_controller.rb
+++ app/controllers/public/communities_controller.rb
@@ -1,2 +1,40 @@
 class Public::CommunitiesController < ApplicationController
+  
+  def index
+    @communities = Community.all.order(created_at: :desc).page(params[:page])
+  end
+  
+  def show
+    @community = Community.find(params[:id])
+  end
+  
+  def new
+    @community = Community.new
+  end
+  
+  def edit
+    @community = Community.find(params[:id])
+  end
+  
+  def create
+    @community = Community.new(community_params)
+    if @community.save
+      flash[:info] = "新規communityを作成しました！"
+      redirect_to public_communities_path
+    else
+      render "new"
+    end
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+  def community_params
+    params.require(:community).permit(:image, :name, :introduction)
+  end
+  
 end
