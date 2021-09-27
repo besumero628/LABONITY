5e3800ef973b92599e2b7e22077750235eb4a048
besumero628 <besumero628@gmail.com>
2021-09-27 12:47:19 +0000

    setting

--- app/controllers/public/top_controller.rb
+++ app/controllers/public/top_controller.rb
@@ -1,7 +1,8 @@
 class Public::TopController < ApplicationController
   def index
     @news = News.where(release_at: DateTime.new..Time.current).order(release_at: :desc).limit(5)
-    @papers = Paper.order("RANDOM()").limit(10)
+    # @papers = Paper.order("RANDOM()").limit(10)
+    @papers = Paper.all.limit(10)
     @communities = Community.order("RANDOM()").limit(10)
     @events = Event.order("RANDOM()").limit(10)
   end
