b20a7b80e18d172afa7616b741912fda4be6203b
besumero628 <besumero628@gmail.com>
2021-10-23 14:48:48 +0000

    ajax favorite

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,2 +1,23 @@
 class Public::FavoritesController < ApplicationController
+
+  def create
+    @favorite = Favorite.new(deliverable_type: params[:type], deliverable_id: params[:id], user_id: current_user.id)
+    @paper = Paper.find(params[:id])
+
+    if !@favorite.save
+      flash[:danger] = "不明なエラーです"
+      redirect_to session[:previous_url]
+    end
+  end
+  
+  def destroy
+    @favorite = Favorite.find_by(deliverable_type: params[:type], deliverable_id: params[:id], user_id: current_user.id)
+    @paper = Paper.find(params[:id])
+    
+    if !@favorite.destroy
+      flash[:danger] = "不明なエラーです"
+      redirect_to session[:previous_url]
+    end
+  end
+
 end
